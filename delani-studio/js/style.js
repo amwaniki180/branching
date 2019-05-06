02c644f3709bb12b99fd8917de3fb80f95aeaf46
Antony <amwaniki180@gmail.com>
2019-05-06 11:31:32 +0300

    development

--- delani-studio/js/style.js
+++ delani-studio/js/style.js
@@ -9,25 +9,3 @@ $("#design").click(funnction)(){
 });
 });
 });
-$(document).ready(function(){
-$("#image2").click(function(){
-$("#devel").show();
-$("#image2").hide();
-
-$("#devel").click(funnction)(){
-  $("#image2").show();
-  $("#devel").hide();
-});
-});
-});
-$(document).ready(function(){
-$("#image3").click(function(){
-$("#product").show();
-$("#image3").hide();
-
-$("#product").click(funnction)(){
-  $("#image3").show();
-  $("#product").hide();
-});
-});
-});
